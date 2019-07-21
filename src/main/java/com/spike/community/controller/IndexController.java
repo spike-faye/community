1ed7485c50e0b76e8f3526eaf2ee8305b849212d
spike-faye <liyizhe2233@163.com>
2019-07-21 11:18:18 +0800

    add check cookies

--- src/main/java/com/spike/community/controller/IndexController.java
+++ src/main/java/com/spike/community/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.spike.community.controller;
 
+import com.spike.community.mapper.UserMapper;
+import com.spike.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * created by
@@ -11,8 +17,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+
+        //获得cookie数组
+        Cookie[] cookies = request.getCookies();
+        //得到包含token键值对的cookie，取其token值
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
--- src/main/java/com/spike/community/mapper/UserMapper.java
+++ src/main/java/com/spike/community/mapper/UserMapper.java
@@ -3,9 +3,15 @@ package com.spike.community.mapper;
 import com.spike.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("INSERT INTO user (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insertUser(User user);
+
+    @Select("SELECT * FROM user WHERE token= #{token}")
+    User findByToken(@Param("token") String token);
+
 }
