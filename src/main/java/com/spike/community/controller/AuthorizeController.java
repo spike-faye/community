93b02da6ca94de118e596efc0cd93c9a77b9e3b8
spike-faye <liyizhe2233@163.com>
2019-08-07 17:24:16 +0800

    追加日志

--- src/main/java/com/spike/community/controller/AuthorizeController.java
+++ src/main/java/com/spike/community/controller/AuthorizeController.java
@@ -6,6 +6,7 @@ import com.spike.community.mapper.UserMapper;
 import com.spike.community.model.User;
 import com.spike.community.provider.GithubProvider;
 import com.spike.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
 
     @Autowired
@@ -60,6 +62,7 @@ public class AuthorizeController {
             return "redirect:/";
         }
         else{
+            log.error("callback get github error，{}",githubUser);
             //登录失败,重新登陆
             return "redirect:/";
         }
