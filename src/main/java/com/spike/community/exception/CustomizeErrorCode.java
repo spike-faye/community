85a5c9e25a6dd8e315a103adfe391bd14a5eaf47
spike-faye <liyizhe2233@163.com>
2019-08-28 12:26:47 +0800

    改为本地mysql

--- src/main/java/com/spike/community/exception/CustomizeErrorCode.java
+++ src/main/java/com/spike/community/exception/CustomizeErrorCode.java
@@ -5,7 +5,7 @@ public enum CustomizeErrorCode implements ICustomizeErrorCode {
     QUESTION_NOT_FOUNT(2001,"你找的问题不存在，请换一个试试"),
     TARGET_PARAM_NOT_FOUNT(2002,"未选中任何问题或评论进行回复"),
     NO_LOGIN(2003,"当前操作需要登录，请登录后重试"),
-    SYSTEM_ERROR(2004,"服务烧着了，不然您稍后试试？"),
+    SYSTEM_ERROR(2004,"服务器烧着了，不然您稍后试试？"),
     TYPE_PARAM_WRONG(2005,"评论类型错误或不存在"),
     COMMENT_NOT_FOUND(2006,"您回复的评论已经不存在"),
     QUESTION_NOT_FOUND(2007,"您回复的问题已经不存在"),
